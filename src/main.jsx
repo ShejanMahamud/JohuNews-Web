cad91eddd8489c61ba42b1edc28da1a5cb6b3cac
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-04-04 13:08:47 +0600

    project setup

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
